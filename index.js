6c4f17e576a44a2d2ee29e549074c45f6733289b
Chris Rowe <chris@webcoda.co.uk>
2015-12-18 15:22:00 +0000

    adding santa hat

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var s3 = new aws.S3({ apiVersion: '2006-03-01' });
 var fs = require('fs');
 var gm = require('gm').subClass({imageMagick: true});
 var Promise = require('promise');
+var cv = require('opencv');
 
 var context, bucket, key;
 var sourceDir = 'originals/';
@@ -34,20 +35,135 @@ var getImage = function(){
     });
 }
 
+var addHat = function(imageBuffer, face){
+    return new Promise(function (resolve, reject) {
+      
+        var hatSize = face.width * 1.3;
+
+        var y = face.y + (face.height * 0.2) - hatSize;
+        if(y < 0){
+            y = y.toString();
+        }else{
+            y = '+' + y;
+        }
+        var x = face.x - (face.width * 0.15);
+        if(x < 0){
+            x = x.toString();
+        }else{
+            x = '+' + x;
+        }
+
+        var geometry = hatSize + 'x' + hatSize + x + y;
+
+        console.log('geometry: ', geometry);
+
+        gm(imageBuffer)
+        //.resize(200, 200)
+        .composite('santahat.png')
+        .geometry(geometry)
+        .toBuffer(function(err, buffer){
+          if (err){
+            console.log('Add santa hat fail', err);
+            reject(err);
+          }else{
+            resolve(buffer);
+          }
+        });
+
+
+        // gm('santahat.png')
+        //     .resize(hatSize, hatSize)
+        //     .write('temphat.png', function(err){
+        //         if (err){
+        //             context.fail("HAT RESIZE FAIL DETECT");
+        //         }else{
+
+                    // var y = face.y - hatSize;
+                    // if(y < 0){
+                    //     y = y.toString();
+                    // }else{
+                    //     y = '+' + y;
+                    // }
+
+                    // var geometry = '+' + face.x + y;
+
+        //             gm(imageBuffer)
+        //             //.resize(200, 200)
+        //             .composite('temphat.png')
+        //             .geometry(geometry)
+        //             .toBuffer(function(err, buffer){
+        //               if (err){
+        //                 console.log('Add santa hat fail', err);
+        //                 reject(err);
+        //               }else{
+        //                 resolve(buffer);
+        //               }
+        //             });
+        //         }
+        //     })
+
+
+    });
+}
+
 var convertImage = function(imageBody){
     console.log('Convert Image');
 
-    gm(imageBody)
-        .composite('santahat.png')
-        .geometry('+100+150')
-        //.resize(200, 200)
-        .toBuffer(function(err, buffer){
-            if (err){
-            context.fail("GM FAIL");
-          }else{
-            saveImage(buffer);
-          }
-        });
+    cv.readImage(imageBody, function(err, im){
+      if (err){
+            ontext.fail("CV FAIL READ IMAGE");
+       }
+      if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
+
+      im.detectObject("node_modules/opencv/data/haarcascade_frontalface_alt.xml", {}, function(err, faces){
+        if (err){
+            context.fail("CV FAIL DETECT");
+        }
+
+        // for (var i = 0; i < faces.length; i++){
+        //   var face = faces[i];
+        //   im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
+        // }
+
+        console.log('Faces found: ', faces.length);
+
+        var face = faces[0];
+
+        addHat(imageBody, face).then(function(imageBuffer){
+            saveImage(imageBuffer);
+        }, function(err){
+            context.fail("ADD HAT FAIL");
+        })
+
+        // var geometry = '+' + face.x + '+' + face.y;
+        // console.log('geometry: ', geometry);
+
+        // gm(imageBody)
+        // .composite('santahat.png')
+        // .geometry('+' + face.x + '+' + face.y)
+        // //.resize(200, 200)
+        // .toBuffer(function(err, buffer){
+        //     if (err){
+        //     context.fail("GM FAIL");
+        //   }else{
+        //     saveImage(buffer);
+        //   }
+        // });
+
+      });
+    });
+
+    // gm(imageBody)
+    //     .composite('santahat.png')
+    //     .geometry('+100+150')
+    //     //.resize(200, 200)
+    //     .toBuffer(function(err, buffer){
+    //         if (err){
+    //         context.fail("GM FAIL");
+    //       }else{
+    //         saveImage(buffer);
+    //       }
+    //     });
 
     // readFile('santahat.png').then(function(fileContents){
     //     console.log('santa hat loaded');
